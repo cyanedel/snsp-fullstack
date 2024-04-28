93b545f29095ce4606577374825f19e9605ef2d1
cyanedel <cleewlkr@gmail.com>
2024-04-29 03:55:28 +0700

    Fixture update;

--- helper/gen_fixture.py
+++ helper/gen_fixture.py
@@ -3,7 +3,7 @@ import json
 data = []
 for i in range(1, 300001):
   id_str = f"{i:06d}"
-  data.append({"model": "members.MemberCardNo", "fields": {"cardnumber": id_str}})
+  data.append({"model": "emp.MasterBadge", "fields": {"badge_no": id_str, "reserved": False}})
 
 with open("initial_no.json", "w") as f:
   json.dump(data, f, indent=2)
